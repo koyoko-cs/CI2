93ba6e4b46a1b8a28d3de8bf0430ad6998e7209a
koyoko-cs <azwan.fpl2@gmail.com>
2024-09-09 14:49:26 +0800

    Update python-job-dsl.groovy

--- python-job-dsl.groovy
+++ python-job-dsl.groovy
@@ -3,7 +3,7 @@ job('Python-Build-Job') {
 
     // Source Code Management
     scm {
-        git('https://github.com/your-repo/python-project.git')
+        git('https://github.com/koyoko-cs/CI2.git')
     }
 
     // Build Triggers
